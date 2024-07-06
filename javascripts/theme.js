48d3c1af27c06549157245047024ee0baecdd3da
pebloop <pebloop@outlook.com>
2024-07-06 19:59:48 +0200

    fix: reverse header and top menu

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -2,7 +2,9 @@ var topMenu = document.getElementByID('top-menu');
 var header = document.getElementByID('header');
 
 // wrap topmenu and header in a div to be able to reverse them
+var parent = topMenu.parentNode;
 var wrapper = document.createElement('div');
 wrapper.setAttribute('id', 'top-wrapper');
+parent.replaceChild(wrapper, topMenu);
 wrapper.appendChild(topMenu);
 wrapper.appendChild(header);
